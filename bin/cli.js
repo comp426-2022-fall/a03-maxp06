542249058469ddf74b96f85765762125c1bc0273
Maxwell Peng <maxp06@email.unc.edu>
2022-11-29 22:35:15 -0500

    3

---
+++ bin/cli.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+
+import {roll} from "../lib/roll.js"
+import parseArgs from 'minimist';
+
+var argv = parseArgs(process.argv);
+var r = roll(argv.sides, argv.dice, argv.rolls);
+
+const obj = {sides: argv.sides, dice: argv.dice, rolls: argv.rolls, results: r};
+console.log(JSON.stringify(obj));
